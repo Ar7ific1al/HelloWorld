157cc01fc16602c97c80deeb7dc2ee96617ccdc0
Nicholas Jackson <ar7ific1al@gmail.com>
2016-11-24 02:41:48 -0600

    Signed-off-by: Nicholas Jackson <ar7ific1al@gmail.com>

--- src/com/mythton/java/helloworld/HelloWorld.java
+++ src/com/mythton/java/helloworld/HelloWorld.java
@@ -2,7 +2,6 @@ package com.mythton.java.helloworld;
 
 public class HelloWorld {
 
-	//	Test commit from eclipse.	-njacks
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
